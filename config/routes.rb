0766bd2e8287f65e748b7cd5cf1764e1d7638eca
smhammadzaidi <smhammadzaidi@gmail.com>
2025-03-13 21:20:55 +0000

    Final Rails static dice roll

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
+  get "/", controller: "pages", action: "home"
 
-  # This is a blank app! Pick your first screen, build out the RCAV, and go from there. E.g.:
+  get "dice/2/6", controller: "dice", action: "two_six"
 
-  # get "/your_first_screen" => "pages#first"
-  
+  get "dice/2/10", controller: "dice", action: "two_ten"
+
+  get "dice/1/20", controller: "dice", action: "one_twenty"
+
+  get "dice/5/4", controller: "dice", action: "five_four"
+
 end
